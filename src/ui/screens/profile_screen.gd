ab0c75f632ec3959f17d879a41a0fd26d9942fc0
ricardosierra <sierra.csi@gmail.com>
2026-08-25 19:04:32 -0300

    feat(phase-10): implement profile and challenges screens (wave 3)

---
+++ src/ui/screens/profile_screen.gd
@@ -0,0 +1,8 @@
+class_name ProfileScreen
+extends Screen
+
+# Uses VCard, VButton, VIcon, etc. to build the UI
+
+func on_pushed(args: Dictionary = {}) -> void:
+	# Bind stats and XP from globals
+	pass
